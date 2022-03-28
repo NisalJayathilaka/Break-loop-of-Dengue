e252d042d66fd29649756e7d804303491ac36cb7
NisalJayathilaka <nisal7075399@gmail.com>
2022-03-28 18:09:33 +0530

    creating the table

--- FrontEnd/BreakLoopD/breakDengeL/src/app/app.component.ts
+++ FrontEnd/BreakLoopD/breakDengeL/src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  template: `
+  <div>
+  <h1>{{title}}</h1>
+  <pm-dengueCases></pm-dengueCases>
+  </div>
+  `
 })
 export class AppComponent {
   title = 'breakDengeL';
--- FrontEnd/BreakLoopD/breakDengeL/src/app/app.module.ts
+++ FrontEnd/BreakLoopD/breakDengeL/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DenguelistComponent } from './dengueCases/dengueCase-list.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DenguelistComponent
   ],
   imports: [
     BrowserModule
