55165302b102338ace63caaaba15b7a2ceace8cf
NisalJayathilaka <nisal7075399@gmail.com>
2022-03-29 06:24:01 +0530

    creating show and hide image

--- FrontEnd/BreakLoopD/breakDengeL/src/app/dengueCases/dengueCase-list.components.ts
+++ FrontEnd/BreakLoopD/breakDengeL/src/app/dengueCases/dengueCase-list.components.ts
@@ -6,5 +6,33 @@ import { Component } from "@angular/core";
 })
 
 export class DenguelistComponent{
+   dangueCases:string="Dengue Cases";
+   imageWidh:number=50;
+   imageMargin:number=2;
+   showImage:boolean=false;
+   ceases:any[] =[ {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2021",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/dengue1.jpeg"
+  },
+  {
+    "productId": 2,
+    "productName": "Garden Cart",
+    "productCode": "GDN-0023",
+    "releaseDate": "March 18, 2021",
+    "description": "15 gallon capacity rolling garden cart",
+    "price": 32.99,
+    "starRating": 4.2,
+    "imageUrl": "assets/images/garden_cart.png"
+  }
+];
 
+toggleImage():void{
+    this.showImage= !this.showImage;
+}
 }
