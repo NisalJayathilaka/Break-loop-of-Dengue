494aab563b7e2adc4e30431232773004a6f6b8fc
NisalJayathilaka <nisal7075399@gmail.com>
2022-03-31 11:18:28 +0530

    getting data to the case deatils page

--- FrontEnd/BreakLoopD/breakDengeL/src/app/dengueCases/dengue-cases-details.component.ts
+++ FrontEnd/BreakLoopD/breakDengeL/src/app/dengueCases/dengue-cases-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ICases } from './dCases';
 
 @Component({
   templateUrl: './dengue-cases-details.component.html',
@@ -7,12 +8,25 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class DengueCasesDetailsComponent implements OnInit {
   pageTitle:string = 'case Details';
+  cases:ICases|undefined;
   
   constructor(private route:ActivatedRoute,private router: Router) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'))
     this.pageTitle +=` ${id} `
+    this.cases=
+      {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2021",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "assets/images/dengue1.jpeg"
+      }
+    
   }
 
   onBack():void{
